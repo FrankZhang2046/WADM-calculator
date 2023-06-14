8a28591a929087c1ce8517d934724f4eec9641dc
Frank Zhang <frankbusinessmail@gmail.com>
2023-06-14 16:34:49 -0400

    introduced ng-chart2 as a dependency

--- src/app/components/custom-html-table/custom-html-table.component.ts
+++ src/app/components/custom-html-table/custom-html-table.component.ts
@@ -1,3 +1,7 @@
+import DataLabelsPlugin from "chartjs-plugin-datalabels";
+import { BaseChartDirective } from "ng2-charts";
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from "chart.js";
+import { NgChartsModule } from "ng2-charts";
 import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 import { Component, HostListener, OnInit, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
@@ -22,6 +26,7 @@ import { AssertArrayEqualityPipe } from "../../pipes/assert-array-equality.pipe"
   selector: "app-custom-html-table",
   standalone: true,
   imports: [
+    NgChartsModule,
     AssertArrayEqualityPipe,
     MatDialogModule,
     CommonModule,
@@ -35,6 +40,56 @@ import { AssertArrayEqualityPipe } from "../../pipes/assert-array-equality.pipe"
   styleUrls: ["./custom-html-table.component.scss"],
 })
 export class CustomHtmlTableComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  public barChartOptions: ChartConfiguration["options"] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: "end",
+        align: "end",
+      },
+    },
+  };
+  public barChartType: ChartType = "bar";
+  public barChartPlugins = [DataLabelsPlugin];
+  public barChartData: ChartData<"bar"> = {
+    labels: ["2006", "2007", "2008", "2009", "2010", "2011", "2012"],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A" },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B" },
+    ],
+  };
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
   public tableOperationConstants = TableOperationConstants;
   public columnToDelete!: number | null;
   public rowToDelete!: number | null;
