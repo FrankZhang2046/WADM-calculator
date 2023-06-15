281b0a6f6113e74fa16e2a2ccf0e2e6cb449c61b
Frank Zhang <frankbusinessmail@gmail.com>
2023-06-15 09:48:10 -0400

    disable cdkDrag when result has been calculated

--- src/app/components/custom-html-table/custom-html-table.component.ts
+++ src/app/components/custom-html-table/custom-html-table.component.ts
@@ -63,13 +63,7 @@ export class CustomHtmlTableComponent implements OnInit {
   };
   public barChartType: ChartType = "bar";
   public barChartPlugins = [DataLabelsPlugin];
-  public barChartData: ChartData<"bar"> = {
-    labels: ["2006", "2007", "2008", "2009", "2010", "2011", "2012"],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A", stack: "a" },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B", stack: "a" },
-    ],
-  };
+  public barChartData!: ChartData<"bar">;
   public chartWidth!: string;
   // events
   public chartClicked({
@@ -178,6 +172,9 @@ export class CustomHtmlTableComponent implements OnInit {
         }
         break;
       case "/":
+        if (this.displayResults) {
+          return;
+        }
         // * initiate table traversal
         if (this.cachedHighlightedTableElementIdx.tableElement === null) {
           this.highlightTableElement(
@@ -241,7 +238,10 @@ export class CustomHtmlTableComponent implements OnInit {
    */
   public tableTraversal(keystroke: string): void {
     // * abort table traversal if user is editing a field
-    if (this.modifiedTableElementIdx.tableElement !== null) {
+    if (
+      this.modifiedTableElementIdx.tableElement !== null ||
+      this.displayResults
+    ) {
       return;
     }
     if (this.highlightedTableElementIdx.tableElement === null) {
@@ -568,6 +568,9 @@ export class CustomHtmlTableComponent implements OnInit {
     tableElement: TableOperationConstants,
     idx: number[]
   ) {
+    if (this.displayResults) {
+      return;
+    }
     this.clearHighlightedTableElement();
     this.modifiedTableElementIdx = { tableElement, idx };
     // have to defer the action to the next loop otherwise the ViewChild will be undefined
@@ -588,6 +591,9 @@ export class CustomHtmlTableComponent implements OnInit {
     tableElement: TableOperationConstants,
     idx: number[]
   ): void {
+    if (this.displayResults) {
+      return;
+    }
     // this should be good enough to add the highlight class to the table element
     this.highlightedTableElementIdx = { tableElement, idx };
     // * cache the highlighted table element so that it can be reactivated
