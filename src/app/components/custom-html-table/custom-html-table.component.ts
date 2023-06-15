97b9dbab2e0d9bc523da0124bd97767c871773fb
Frank Zhang <frankbusinessmail@gmail.com>
2023-06-15 08:40:50 -0400

    implemented table sizing when hit calculate button

--- src/app/components/custom-html-table/custom-html-table.component.ts
+++ src/app/components/custom-html-table/custom-html-table.component.ts
@@ -65,10 +65,11 @@ export class CustomHtmlTableComponent implements OnInit {
   public barChartData: ChartData<"bar"> = {
     labels: ["2006", "2007", "2008", "2009", "2010", "2011", "2012"],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A" },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B" },
+      { data: [65, 59, 80, 81, 56, 55, 40], label: "Series A", stack: "a" },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B", stack: "a" },
     ],
   };
+  public chartWidth!: string;
   // events
   public chartClicked({
     event,
@@ -604,6 +605,9 @@ export class CustomHtmlTableComponent implements OnInit {
       }, 0);
     });
     this.displayResults = true;
+    const tableEle = document.querySelector("table");
+    const tableWidth = tableEle?.offsetWidth ? tableEle.offsetWidth : 0;
+    this.chartWidth = (tableWidth - 48).toString() + "px";
   }
 
   private initiateTableTraversal() {
