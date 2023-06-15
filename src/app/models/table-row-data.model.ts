30129c9fc2153d85d9943df51a381f1ea09b6a0b
Frank Zhang <frankbusinessmail@gmail.com>
2023-06-15 18:01:24 -0400

    added error and success message display

--- src/app/models/table-row-data.model.ts
+++ src/app/models/table-row-data.model.ts
@@ -1,11 +1,11 @@
 export interface TableRowData {
-    fieldName: string;
-    // fieldValues is an array consisting of numbers, or null values
-    fieldValues: (number | null)[];
-    fieldWeight: number | null;
+  fieldName: string;
+  // fieldValues is an array consisting of numbers, or null values
+  fieldValues: (number | null)[];
+  fieldWeight: number | null;
 }
 
 export interface ColumnHeaderData {
-    columnName: string;
-    result: number | null;
+  columnName: string;
+  result: number;
 }
