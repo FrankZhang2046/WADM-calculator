2929553d3bd1c882fc1a52a3dea8b2cc03e16cbe
Frank Zhang <frankbusinessmail@gmail.com>
2023-06-17 18:16:50 -0400

    able to sign in and sign out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,6 @@
+import { MatMenuModule } from "@angular/material/menu";
+import { TableStateModel } from "./stores/states/table.state";
+import { AuthStateModel } from "./stores/states/auth.state";
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Router, RouterOutlet } from "@angular/router";
@@ -7,14 +10,16 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
-import { Auth, onAuthStateChanged } from "@angular/fire/auth";
-import { Store } from "@ngxs/store";
+import { Auth, User, onAuthStateChanged, signOut } from "@angular/fire/auth";
+import { Select, Store } from "@ngxs/store";
 import { AuthActions } from "./stores/actions/user.action";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [
+    MatMenuModule,
     MatTooltipModule,
     CommonModule,
     RouterOutlet,
@@ -28,6 +33,11 @@ import { AuthActions } from "./stores/actions/user.action";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
+  @Select(
+    (state: { user: AuthStateModel; table: TableStateModel }) =>
+      state.user.currentUser
+  )
+  currentUser$!: Observable<User | null>;
   constructor(
     private router: Router,
     private auth: Auth,
@@ -39,6 +49,12 @@ export class AppComponent implements OnInit {
   public ngOnInit(): void {
     onAuthStateChanged(this.auth, (user) => {
       this.store.dispatch(new AuthActions.RegisterCurrentUser(user));
+      if (user) {
+        this.redirectMethod("/");
+      }
     });
+  }
+  public logOut() {
+    signOut(this.auth);
   }
 }
