622adb0fb6758b195c501f0b676ca51dd4570e1e
Frank Zhang <frankbusinessmail@gmail.com>
2023-07-03 13:48:53 -0400

    working on conditionally display tutorial modal

--- src/app/stores/states/app.state.ts
+++ src/app/stores/states/app.state.ts
@@ -1,15 +1,19 @@
-import { Injectable } from "@angular/core";
-import { Action, State, StateContext } from "@ngxs/store";
-import { ApplicationActions } from "../actions/app.action";
+import {Injectable} from "@angular/core";
+import {Action, State, StateContext} from "@ngxs/store";
+import {ApplicationActions} from "../actions/app.action";
 
 export interface ApplicationStateModel {
   appState: "tutorial" | "work" | null;
+  dismissTutorialPermanently: boolean;
+  dismissTutorialForSession: boolean;
 }
 
 @State<ApplicationStateModel>({
   name: "application",
   defaults: {
     appState: null,
+    dismissTutorialForSession: false,
+    dismissTutorialPermanently: false,
   },
 })
 @Injectable()
@@ -21,6 +25,26 @@ export class ApplicationState {
   ): void {
     ctx.patchState({
       appState: action.payload,
+    });
+  }
+
+  @Action(ApplicationActions.UpdateTutorialDismissedPermanently)
+  updateTutorialDismissedPermanently(
+    ctx: StateContext<ApplicationStateModel>,
+    action: ApplicationActions.UpdateTutorialDismissedPermanently
+  ): void {
+    ctx.patchState({
+      dismissTutorialPermanently: action.payload,
+    });
+  }
+
+  @Action(ApplicationActions.UpdateTutorialDismissedForSession)
+  updateTutorialDismissedForSession(
+    ctx: StateContext<ApplicationStateModel>,
+    action: ApplicationActions.UpdateTutorialDismissedForSession
+  ): void {
+    ctx.patchState({
+      dismissTutorialForSession: action.payload,
     });
   }
 }
