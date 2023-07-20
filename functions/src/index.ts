a2911e1c4435e888dd9001db30b8c98bbbfb2816
Frank Zhang <frankbusinessmail@gmail.com>
2023-07-20 14:45:21 -0400

    prototyped image optimization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,9 +10,12 @@
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as sharp from "sharp";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
@@ -22,5 +25,18 @@ export const helloWorld = onRequest((request, response) => {
 export const optimizeProfileImage = functions.storage
   .object()
   .onFinalize(async (object) => {
-    logger.info("image optimization function triggered");
+    if (object.contentType === "image/webp") {
+      return;
+    }
+    const bucket = admin.storage().bucket(object.bucket);
+    const filePath = object.name;
+    const file = bucket.file(filePath!);
+    logger.info(`file metadata: `, file.metadata);
+    logger.info(`file path: `, filePath);
+    const [buffer] = await file.download();
+    const optimizedBuffer = await sharp(buffer)
+      .resize(200, 200, { fit: "inside", withoutEnlargement: true })
+      .webp({ quality: 90 })
+      .toBuffer();
+    await file.save(optimizedBuffer, { contentType: "image/webp" });
   });
